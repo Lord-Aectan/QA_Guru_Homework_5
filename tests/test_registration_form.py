5c17fb9ddc17025038c4f13e34ab2267c62f1b25
Daniil Moiseenko <daniilmoiseenko@gmail.com>
2023-04-16 13:29:23 +0300

    Заполнил форму до ввода номера тедефона включительно

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -3,4 +3,15 @@ from tests.conftest import browser_open_page
 
 
 def test_registration_form():
-    browser_open_page()
+    browser.config.window_width = 1500
+    browser.config.window_height = 1200
+    browser.open('https://demoqa.com/automation-practice-form')
+
+    browser.element('#firstName').type('Test')
+    browser.element('#lastName').type('Test2')
+    browser.element('#userEmail').type('test@test.test')
+    browser.element("#gender-radio-1").double_click()
+    browser.element('#userNumber').type('78484884844')
+    browser.element()
+    browser.element()
+    browser.element()
