f7037c0e022d9520d40c97dd3cfb0a7c9956df1f
Daniil <38397783+Lord-Aectan@users.noreply.github.com>
2023-04-19 12:45:12 +0300

    Delete conftest.py
    Не используемый файл

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-from selene import browser, by, be, have
-
-
-def browser_open_page():
-    browser.config.window_width = 1500
-    browser.config.window_height = 1200
-    browser.open('https://demoqa.com/automation-practice-form')
-
